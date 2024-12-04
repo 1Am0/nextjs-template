25d901b97d1806df430fad92aa4e0d2ea0c2573a
Anthony Mokhov <131702385+1Am0@users.noreply.github.com>
2024-12-04 04:21:57 +0000

    Added 4 page

---
+++ app/components/invert/invert.jsx
@@ -0,0 +1,26 @@
+'use client'
+
+import styles from "./invert.module.css";
+import { useState, useEffect } from "react";
+
+export default function Invert() {
+
+  const [state, setState] = useState(false);
+
+  useEffect(() => {
+    console.log(`useEffect callback from change to state (state is now ${state})`);
+  }, [state]);
+
+  function invertState(){
+    setState(true);
+  }
+
+  return (
+    <div className={styles.counter}>
+      <div className={styles.row}>
+        <button onClick={() => invertState()}>Invert</button>
+        <div className={styles.display}>{String(state)}</div>
+      </div>
+    </div>
+  );
+}
---
+++ app/page4/page.jsx
@@ -0,0 +1,10 @@
+import styles from "./page.module.css";
+import Invert from "../components/invert/invert.jsx";
+
+export default function Page3() {
+  return (
+    <div className={styles.page}>
+      <Invert/>
+    </div>
+  );
+}
